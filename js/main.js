420dbb7467fb2ff29dd58ad9975f104a83d39e87
denysoleksiienko <42835486+denysoleksiienko@users.noreply.github.com>
2019-09-05 18:33:39 +0300

    fix map icon

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 let form = document.querySelector('.contact__form');
 let btnOpen = document.querySelector('.write-to-us');
 let btnClose = document.querySelector('.contact__form-close')
+let mapIco = document.querySelector('.footer__map-ico');
 
 btnOpen.addEventListener('click', (e) => {
     e.preventDefault();
     form.style.display = "block";
+    mapIco.style.display = "none";
 })
 
 btnClose.addEventListener('click', (e) => {
     e.preventDefault();
     form.style.display = "none";
+    mapIco.style.display = "block";
 })
